943012086b061c2b598d2e7972ed0774a6929c90
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-06-23 19:20:54 +1200

    Test heroku deployment

--- index.js
+++ index.js
@@ -3,35 +3,7 @@ const { buffer, text, json } = require('micro');
 const cors = require('micro-cors')();
 
 const handler = async (req, res) => {
-  const data = await json(req);
-  console.log('Request', data);
-  const projectId = 'voxcode-3b433';
-  const sessionId = 'quickstart-session-id';
-  const languageCode = 'en-US';
-  const sessionClient = new dialogflow.SessionsClient({ keyFilename: 'VoxCode-25315b0b4fa3.json' });
-  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-  const query = data.text;
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: query,
-        languageCode: languageCode
-      }
-    }
-  };
-
-  try {
-    const responses = await sessionClient.detectIntent(request);
-    const result = responses[0].queryResult;
-    console.log('RES', result);
-
-    return result;
-  } catch (err) {
-    console.error('ERROR:', err);
-    return '<div>Hello World!</div>';
-  }
+  return 'hello';
 };
 
 module.exports = cors(handler);
